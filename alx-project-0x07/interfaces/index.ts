ae81f98471c65d4b641f9c89a1206b4005690163
amuMabirimise <awmabirimise@gmail.com>
2025-01-13 16:59:50 +0200

    Advance Image Generation App

--- alx-project-0x07/interfaces/index.ts
+++ alx-project-0x07/interfaces/index.ts
@@ -4,12 +4,3 @@ export interface ReactComponentProps {
   children: React.ReactNode;
 }
 
-export interface GeneratedImageProps {
-  imageUrl: string;
-  prompt: string;
-  width?: string;
-  height?: string;
-  action: (imagePath: string) => void;
-}
-
-export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
